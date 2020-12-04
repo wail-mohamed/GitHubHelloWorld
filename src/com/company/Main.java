47e78955e570d2aa1dce1ed718ae36f36240871c
Wail Yousif <wail.yousif@hotmail.com>
2020-12-04 20:46:27 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    /*
+    How to setup your machine for GitHub.
+    https://docs.github.com/en/free-pro-team@latest/github/getting-started-with-github/set-up-git?email_source=welcome
+
+    You can also download GitHub Desktop 
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
